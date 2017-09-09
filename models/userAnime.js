9a84a6445118e608e800e388855b878b51bad28f
radiantdinosaurs <radiantdinosaurs@gmail.com>
2017-09-09 02:33:46 -0700

    Did a lot of restructuring to code flow with async waterfall for when user posts search. Changed around some of the order of API requests/database queries. Also, started working on adding to relationship tables.

--- models/userAnime.js
+++ models/userAnime.js
@@ -1,19 +1,32 @@
 var Sequelize = require('sequelize');
 var sequelize = require('../data/sequelizeDatabase');
-var Anime = require('./anime');
-var User = require('./user');
 
-var UserAnime = sequelize.define('user_anime', {
-  anime_id: Sequelize.INTEGER,
-  user_id: Sequelize.INTEGER,
-  my_watched_episodes: Sequelize.INTEGER,
-  my_start_date: Sequelize.DATE,
-  my_finish_date: Sequelize.DATE,
-  my_score: Sequelize.INTEGER,
-  my_status: Sequelize.INTEGER,
-  my_rewatching: Sequelize.INTEGER,
-  my_rewatching_episodes: Sequelize.INTEGER
+module.exports = sequelize.define('user_anime', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    fk_anime_id_user_anime: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    fk_user_id_user_anime: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    my_watched_episodes: Sequelize.INTEGER,
+    my_start_date: Sequelize.DATE,
+    my_finish_date: Sequelize.DATE,
+    my_score: Sequelize.INTEGER,
+    my_status: Sequelize.INTEGER,
+    my_rewatching: Sequelize.INTEGER,
+    my_rewatching_episodes: Sequelize.INTEGER
+}, {
+    timestamps: false,
+    underscored: true,
+    freezeTableName: true,
+    tableName: 'user_anime'
 });
-
-Anime.belongsToMany(User, { through: UserAnime});
-User.belongsToMany(Anime, { through: UserAnime});
