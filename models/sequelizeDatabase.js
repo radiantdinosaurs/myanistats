0e10ea07577ceb88e97a7581b56bc6c3621ae421
radiantdinosaurs <radiantdinosaurs@gmail.com>
2017-10-05 23:20:37 -0700

    Started working on testing.

--- models/sequelizeDatabase.js
+++ models/sequelizeDatabase.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('database', 'username', 'password', {
+var sequelize = new Sequelize('database', 'user', 'password', {
   host: 'host',
   dialect: 'dialect',
   port: 'port'
