a503379753fd8c3ea6e029aedb51430441febe59
radiantdinosaurs <radiantdinosaurs@gmail.com>
2018-04-19 20:01:54 -0700

    Changed the spacing and other minor refactoring.

--- app/models/anime.js
+++ app/models/anime.js
@@ -1,5 +1,5 @@
-var Sequelize = require('sequelize');
-var sequelize = require('./sequelizeDatabase');
+const Sequelize = require('sequelize')
+const sequelize = require('./sequelizeDatabase')
 
 module.exports = sequelize.define('anime', {
     id: {
@@ -8,68 +8,67 @@ module.exports = sequelize.define('anime', {
         allowNull: false,
         primaryKey: true,
         unique: true
-  },
-  link_canonical: {
-    type: Sequelize.STRING
-  },
-  synopsis: {
-    type: Sequelize.TEXT
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  image: {
-    type: Sequelize.STRING
-  },
-  synonyms: {
-    type: Sequelize.TEXT
-  },
-  type: {
-      type: Sequelize.STRING
-  },
-  episodes: {
-    type: Sequelize.INTEGER
-  },
-  status: {
-    type: Sequelize.STRING
-  },
-  aired: {
-    type: Sequelize.STRING
-  },
-  premiered: {
-    type: Sequelize.STRING
-  },
-  source: {
-    type: Sequelize.STRING
-  },
-  duration: {
-    type: Sequelize.STRING
-  },
-  rating: {
-    type: Sequelize.STRING
-  },
-  score: {
-    type: Sequelize.INTEGER
-  },
-  number_of_votes: {
-    type: Sequelize.INTEGER
-  },
-  ranked: {
-    type: Sequelize.INTEGER
-  },
-  popularity: {
-    type: Sequelize.INTEGER
-  },
-  members: {
-    type: Sequelize.INTEGER
-  },
-  favorites: {
-    type: Sequelize.INTEGER
-  }
-}, {
-  timestamps: false,
-  underscored: true,
-  freezeTableName: true,
-  tableName: 'anime'
-});
+    },
+    link_canonical: {
+        type: Sequelize.STRING
+    },
+    synopsis: {
+        type: Sequelize.TEXT
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    image: {
+        type: Sequelize.STRING
+    },
+    synonyms: {
+        type: Sequelize.TEXT
+    },
+    type: {
+        type: Sequelize.STRING
+    },
+    episodes: {
+        type: Sequelize.INTEGER
+    },
+    status: {
+        type: Sequelize.STRING
+    },
+    aired: {
+        type: Sequelize.STRING
+    },
+    premiered: {
+        type: Sequelize.STRING
+    },
+    source: {
+        type: Sequelize.STRING
+    },
+    duration: {
+        type: Sequelize.STRING
+    },
+    rating: {
+        type: Sequelize.STRING
+    },
+    score: {
+        type: Sequelize.INTEGER
+    },
+    number_of_votes: {
+        type: Sequelize.INTEGER
+    },
+    ranked: {
+        type: Sequelize.INTEGER
+    },
+    popularity: {
+        type: Sequelize.INTEGER
+    },
+    members: {
+        type: Sequelize.INTEGER
+    },
+    favorites: {
+        type: Sequelize.INTEGER
+    }}, {
+    timestamps: false,
+    underscored: true,
+    freezeTableName: true,
+    tableName: 'anime'
+})
