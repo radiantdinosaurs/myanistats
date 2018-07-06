85ce20535357be30f040556105fd6c0bee395bfe
radiantdinosaurs <radiantdinosaurs@gmail.com>
2018-07-06 16:46:57 -0700

    Fixed some ESLint errors.

--- app/ui/public/scripts/script.js
+++ app/ui/public/scripts/script.js
@@ -1,9 +1,8 @@
 $(function() {
-    alert('hi')
-    $('#loading-overlay').hide(); // Hiding the loading overlay
+    $('#loading-overlay').hide() // Hiding the loading overlay
     $('#search-submit-btn').click(function(e) {
-        e.preventDefault();
-        let user = $('#search').val();
+        e.preventDefault()
+        let user = $('#search').val()
         $.ajax({
             type: 'POST',
             url: '/',
@@ -11,11 +10,11 @@ $(function() {
             xhrFields: {withCredentials: false},
             headers: {},
             // beforeSend: function(){$('#loading-overlay').show();},
-            complete: function(){$('#loading-overlay').hide();},
+            complete: function() { $('#loading-overlay').hide() },
             success: function(data) {
-                $('#landing-page-content').hide(); // Hiding any content on the page that isn't user stats
-                let source = $('#user-results').html();
-                let template = Handlebars.compile(source);
+                $('#landing-page-content').hide() // Hiding any content on the page that isn't user stats
+                let source = $('#user-results').html()
+                let template = Handlebars.compile(source)
                 let userStats = template({
                     title: data.name,
                     mean_score: data.mean_score,
@@ -26,66 +25,65 @@ $(function() {
                     most_watched_premiered: data.most_watched_premiered,
                     most_watched_month: data.most_watched_month,
                     most_watched_month_percent: data.most_watched_month_percent
-                });
-                $('.hbs-container').prepend(userStats);
+                })
+                $('.hbs-container').prepend(userStats)
 
-                let userBasicStats = {
-                    labels: [
-                        'Completed',
-                        'Currently Watching',
-                        'Dropped',
-                        'On Hold',
-                        'Plan to Watch'],
-                    datasets: [{
-                        data: [
-                            data.completed,
-                            data.currently_watching,
-                            data.dropped,
-                            data.on_hold,
-                            data.plan_to_watch],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)'],
-                        borderColor: [
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)'],
-                        hoverBackgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)'],
-                        hoverBorderColor: [
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)',
-                            'rgba(250, 250, 250, 1)'],
-                        borderWidth: 1}]
-                };
-                let ctx = document.getElementById("userBasicStatsDoughnutChart");
-                let userBasicStatsDoughnutChart = new Chart(ctx, {
-                    type: 'doughnut',
-                    data: userBasicStats,
-                    options: {
-                        legend: {
-                            position: 'top'
-                        }
-                    }
-                });
-
+                // let userBasicStats = {
+                //     labels: [
+                //         'Completed',
+                //         'Currently Watching',
+                //         'Dropped',
+                //         'On Hold',
+                //         'Plan to Watch'],
+                //     datasets: [{
+                //         data: [
+                //             data.completed,
+                //             data.currently_watching,
+                //             data.dropped,
+                //             data.on_hold,
+                //             data.plan_to_watch],
+                //         backgroundColor: [
+                //             'rgba(255, 99, 132, 0.2)',
+                //             'rgba(54, 162, 235, 0.2)',
+                //             'rgba(255, 206, 86, 0.2)',
+                //             'rgba(75, 192, 192, 0.2)',
+                //             'rgba(153, 102, 255, 0.2)'],
+                //         borderColor: [
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)'],
+                //         hoverBackgroundColor: [
+                //             'rgba(255, 99, 132, 0.2)',
+                //             'rgba(54, 162, 235, 0.2)',
+                //             'rgba(255, 206, 86, 0.2)',
+                //             'rgba(75, 192, 192, 0.2)',
+                //             'rgba(153, 102, 255, 0.2)'],
+                //         hoverBorderColor: [
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)',
+                //             'rgba(250, 250, 250, 1)'],
+                //         borderWidth: 1}]
+                // }
+                // let ctx = document.getElementById('userBasicStatsDoughnutChart')
+                // let userBasicStatsDoughnutChart = new Chart(ctx, {
+                //     type: 'doughnut',
+                //     data: userBasicStats,
+                //     options: {
+                //         legend: {
+                //             position: 'top'
+                //         }
+                //     }
+                // })
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
-                console.log(XMLHttpRequest);
-                console.log(textStatus);
-                console.log(errorThrown);
+                console.log(XMLHttpRequest)
+                console.log(textStatus)
+                console.log(errorThrown)
             }
-        });
-    });
-});
+        })
+    })
+})
